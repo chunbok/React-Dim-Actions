96b6f1827a3554e2318ec6486f9727b915a90dbd
Yoontae Kim <kimchunbok@hotmail.com>
2024-01-07 01:59:50 +0900

    build와 test 수정
    build시 테스트 추가
    simple test 추가

--- test/sample.test.jsx
+++ test/sample.test.jsx
@@ -4,6 +4,8 @@ import { render, unmountComponentAtNode } from "react-dom";
 import { act } from "react-dom/test-utils";
 import { DimArea } from "../src/";
 
+// TOBE transation 관련 테스트가 가능한지 찾은 후 가능하면 delay와 duration을 테스트 해야 한다.
+
 let container = null;
 beforeEach(() => {
   // DOM 엘리먼트를 렌더링 대상으로 설정
@@ -18,19 +20,88 @@ afterEach(() => {
   container = null;
 });
 
-it("renders with or without a name", () => {
+test("Dim 영역이 제대로 동작하는지 확인한다.", () => {
   act(() => {
     render(<DimArea dim={{ active: false }} />, container);
   });
-  expect(container.textContent).toBe("Hey, stranger");
+  expect(container.querySelector("#dimArea").style.display).toEqual("none");
 
   act(() => {
-    render(<DimArea dim={{ active: false }} />, container);
+    render(<DimArea dim={{ active: true }} />, container);
   });
-  expect(container.textContent).toBe("Hello, Jenny!");
+  expect(container.querySelector("#dimArea").style.display).toEqual("block");
+});
 
+test("설정으로 재정의하는 zIndex가 적용되는지 확인한다.", () => {
   act(() => {
     render(<DimArea dim={{ active: false }} />, container);
   });
-  expect(container.textContent).toBe("Hello, Margaret!");
+  expect(container.querySelector("#dimArea").style.zIndex).toEqual("150");
+
+  act(() => {
+    render(<DimArea dim={{ active: false, zIndex: 777 }} />, container);
+  });
+  expect(container.querySelector("#dimArea").style.zIndex).toEqual("777");
+});
+
+test("Dim영역 자체의 클릭이벤트가 정상작동하는지 확인한다.", () => {
+  const clearAction = jest.fn();
+
+  act(() => {
+    render(
+      <DimArea dim={{ active: true, clearAction: clearAction }} />,
+      container
+    );
+  });
+  expect(container.querySelector("#dimArea").style.display).toEqual("block");
+
+  act(() => {
+    container
+      .querySelector("#dimArea")
+      .dispatchEvent(new MouseEvent("click", { bubbles: true }));
+  });
+  expect(clearAction).toHaveBeenCalledTimes(1);
+});
+
+test("Dim 영역의 spread(left와 top)가 정상작동하는지 확인한다.", () => {
+  // 방향이 지정되어 있으면 제원으로만 통제
+  act(() => {
+    render(
+      <DimArea dim={{ active: false, spread: { direction: "left" } }} />,
+      container
+    );
+  });
+  expect(container.querySelector("#dimArea").style.display).toEqual("block");
+  expect(container.querySelector("#dimArea").style.width).toEqual("0vw");
+  expect(container.querySelector("#dimArea").style.height).toEqual("100vh");
+
+  act(() => {
+    render(
+      <DimArea dim={{ active: true, spread: { direction: "left" } }} />,
+      container
+    );
+  });
+  expect(container.querySelector("#dimArea").style.display).toEqual("block");
+  expect(container.querySelector("#dimArea").style.width).toEqual("100vw");
+  expect(container.querySelector("#dimArea").style.height).toEqual("100vh");
+
+  act(() => {
+    render(
+      <DimArea dim={{ active: false, spread: { direction: "top" } }} />,
+      container
+    );
+  });
+  expect(container.querySelector("#dimArea").style.display).toEqual("block");
+  expect(container.querySelector("#dimArea").style.width).toEqual("100vw");
+  expect(container.querySelector("#dimArea").style.height).toEqual("0vh");
+
+  act(() => {
+    render(
+      <DimArea dim={{ active: true, spread: { direction: "top" } }} />,
+      container
+    );
+  });
+  expect(container.querySelector("#dimArea").style.display).toEqual("block");
+  expect(container.querySelector("#dimArea").style.width).toEqual("100vw");
+  expect(container.querySelector("#dimArea").style.height).toEqual("100vh");
 });
